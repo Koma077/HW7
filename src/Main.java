548c0c94ccb3fe049dd0633c79586e70def17d70
daniilakulov <sladkoeshka33@yandex.ru>
2022-08-18 13:10:23 +0700

    Выполнил задание

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
 public class Main {
     public static void main(String[] args) {
+        //Задание 1
+        String firstName = "ivan";
+        String middleName = "ivanovich";
+        String lastName = "ivanov";
+        String fullName = lastName + " " + firstName + " " + middleName;
+        System.out.println("ФИО сотрудника -" + fullName);
+
+       //Задание 2
+        System.out.println("Данные ФИО сотрудника для заполнения отчета -" + fullName.toUpperCase());
+
+        //Задание 3
+        fullName = "Иванов Семён Семёнович";
+        System.out.println(fullName.contains("ё"));
+        System.out.println("Данные ФИО сотруднка -" + fullName.replace("ё", "е"));
+
 
     }
 }
